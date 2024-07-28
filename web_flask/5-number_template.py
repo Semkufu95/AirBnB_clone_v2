f10e91611d93ca6a48c94b147fdecb56ec36b550
semkufu95 <semkufu95@gmail.com>
2024-07-29 01:57:29 +0300

    Tak 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+ A script that starts a flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_bnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def Python_text(text='is cool'):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>')
+def num(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
